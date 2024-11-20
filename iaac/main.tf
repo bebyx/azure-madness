d7840719c15730d309056d0c8b712b2afe76a498
bebyx <artem@bebyx.co.ua>
2024-11-20 15:53:27 +0200

    Enable more dynamic switching of regions

--- iaac/main.tf
+++ iaac/main.tf
@@ -27,9 +27,14 @@ data "azurerm_resource_group" "custom_rg" {
   name = "Artem-Candidate"
 }
 
+locals {
+  //location = data.azurerm_resource_group.custom_rg.location
+  location = "northeurope"
+}
+
 resource "azurerm_key_vault" "key_vault" {
-  name                = "bebyx-secrets"
-  location            = data.azurerm_resource_group.custom_rg.location
+  name                = "bebyx-secrets-v2"
+  location            = local.location
   resource_group_name = data.azurerm_resource_group.custom_rg.name
 
   sku_name = "standard"
@@ -61,7 +66,7 @@ resource "azurerm_key_vault" "key_vault" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "common-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.custom_rg.location
+  location            = local.location
   resource_group_name = data.azurerm_resource_group.custom_rg.name
 }
 
@@ -71,6 +76,8 @@ module "jenkins" {
   resource_group_id = data.azurerm_resource_group.custom_rg.id
   key_vault_id      = azurerm_key_vault.key_vault.id
   vnetwork_name     = azurerm_virtual_network.vnet.name
+
+  resource_group_location = local.location
 }
 
 module "aks" {
@@ -82,6 +89,8 @@ module "aks" {
   tenant_id         = data.azurerm_client_config.current.tenant_id
   object_id         = data.azurerm_client_config.current.object_id
   subscription_id   = data.azurerm_client_config.current.subscription_id
+
+  resource_group_location = local.location
 }
 
 output "vm_public_ip_from_module" {
