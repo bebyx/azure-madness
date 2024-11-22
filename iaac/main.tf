6381d946767ce326d6269462eea49587b9eb8e3e
bebyx <artem@bebyx.co.ua>
2024-11-22 19:52:37 +0200

    Factor out main IaaC outputs

--- iaac/main.tf
+++ iaac/main.tf
@@ -103,27 +103,3 @@ module "aks" {
   dns_zone_id             = azurerm_dns_zone.common.id
   dns_zone_name           = azurerm_dns_zone.common.name
 }
-
-output "vm_public_ip" {
-  value = module.jenkins.vm_ip
-}
-
-output "public_ssh" {
-  value = module.jenkins.key_data
-}
-
-output "ingress_pip" {
-  value = module.aks.ingress_pip
-}
-
-output "acr_client_id" {
-  value = module.aks.acr_sp_client_id
-}
-
-output "aks_client_id" {
-  value = module.aks.aks_sp_client_id
-}
-
-output "acr" {
-  value = module.aks.acr
-}
